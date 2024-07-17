3756e10a02d7f1b0b73f517b88d4730dcd355c87
Programação em Python 2024.4 <36129532024.4@senacrjedu.local>
2024-07-17 12:14:57 -0300

    Primeiro commit

---
+++ exer15.js
@@ -0,0 +1,16 @@
+//  Mostrar o maior entre 3 numero
+
+let numero1 = 10
+let numero2 = 2
+let numero3 = 15
+
+
+if(numero1>=numero2 && numero1 >= numero3){
+    console.log(numero1)
+}
+else if (numero2>numero3){
+    console.log(numero2)
+}
+else{
+    console.log(numero3)
+}
---
+++ if.py
@@ -0,0 +1,19 @@
+# Verificar se é maior de idade
+
+maiorIdade = 18
+idade = int(input('Digite sua idade\n'))
+
+if (idade >= maiorIdade):
+    print('Eh maior de idade')
+else:
+    print('Eh menor de idade')
+
+
+# if (variavel 'sinal' variavel)
+# é igual/diferente?
+# operador Igual: ==
+# operador Diferente: !=
+# é maior/menor?
+# maior : >(Maior ou igual: >=)
+# menor : <(Menor ou igual: <=)
+
